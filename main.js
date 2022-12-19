17d2c4ab383311dbf9d0d19ba83935e73337f650
Miryan Lopez <milobe84@gmail.com>
2022-12-19 15:34:32 -0500

    cambios

--- main.js
+++ main.js
@@ -24,3 +24,36 @@ if (marcosMayorIMC){
     console.log(`El IMC de Juan (${imc2.toFixed (1)}) es mas alto que el de de Marcos (${imc1.toFixed (1)})`);
 }
 
+// en el siguiente codigo se debe identificar quien es el equipo ganador teniendo en cuenta las codiciones del reto
+
+const delfinesPuntuacion1 = 96;
+const delfinesPuntuacion2 = 108;
+const delfinesPuntuacion3 = 89;
+
+const koalasPuntuacion1 = 88;
+const koalasPuntuacion2 = 91;
+const koalasPuntuacion3 = 110;
+
+const promedioDelfines = (delfinesPuntuacion1 + delfinesPuntuacion2 + delfinesPuntuacion3) / 3;
+console.log("promedio Delfines: " + promedioDelfines)
+
+const promedioKoalas = (koalasPuntuacion1 + koalasPuntuacion2 + koalasPuntuacion3) / 3;
+console.log("promedio Koalas: " + promedioKoalas)
+
+if (promedioDelfines >= 100 && promedioDelfines > promedioKoalas){
+    console.log("Ganan los Delfines");
+}else if (promedioKoalas > promedioDelfines && promedioKoalas >= 100){
+    console.log("Ganan los Koalas");
+}else if (promedioDelfines == promedioKoalas && (promedioDelfines >= 100 && promedioKoalas >= 100)){
+    console.log("Hay un empate");
+}else{
+    console.log("Nadie Gana");
+}
+
+
+// en el siguiente codigo se debe identificar la propina y presentar el valor total segun las condiciones del reto.
+
+let propina = 0;
+
+
+
